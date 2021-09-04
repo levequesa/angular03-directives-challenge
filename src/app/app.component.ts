64b4b0491b541f2401013ec4edcb9d124dc6879c
levequesa <79331585+levequesa@users.noreply.github.com>
2021-09-04 11:35:02 +0200

    ng - les directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular03-directives-challenge';
+  bonjourTitle : string = 'Bonjour !';
+  isAdmin : boolean = true;
+
 }
